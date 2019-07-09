7f79137a33419a0e00c50588e1c4c4ebea073486
fernandoteixeira <fernandoadt1@gmail.com>
2019-07-09 17:29:16 -0300

    Continue to structure allure report and create first test

---
+++ test/step-definitions/ta-login.steps.js
@@ -0,0 +1,11 @@
+const { Given,Then,When } = require('cucumber');
+
+Given(/^I am on the phptravels page$/, function() {
+});
+
+When(/^I login with username and password "user@phptravels.com" "demouser" into the text box$/, function() {
+});
+
+Then(/^I should see the message "Hi, Johny Smith" on my account page$/, function() {
+    
+  });
--- wdio.conf.js
+++ wdio.conf.js
@@ -2,18 +2,24 @@ exports.config = {
     debug: process.env.DEBUG === '1',
     execArgv: process.env.DEBUG === '1' ? ['--inspect-brk=127.0.0.1:5859'] : [],
     specs: [
-        './features/**/*.feature'
+        './test/features/**/*.feature'
     ],
     exclude: [],
     maxInstances: 10,
-    capabilities: [{
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
+    capabilities: [
+       {
         maxInstances: 5,
-        //
-        browserName: 'firefox'
-    }],
+        browserName: 'firefox',
+        "moz:firefoxOptions": { args: ['-headless'] }
+       },
+       {
+        maxInstances: 5,
+        browserName: 'chrome',
+        chromeOptions: {
+            args: ['--headless'],
+        }
+       }
+    ],
     sync: true,
     logLevel: 'error',
     coloredLogs: true,
@@ -31,11 +37,11 @@ exports.config = {
         allure: {
             outputDir:   './test/reports/allure-results/',
             disableWebdriverStepsReporting: false,
-            useCucumberStepReporter: false,
+            useCucumberStepReporter: true,
           },
     },
     cucumberOpts: {
-        require: ['./features/step-definitions'],        // <string[]> (file/dir) require files before executing features
+        require: ['./test/step-definitions/*.js'],        // <string[]> (file/dir) require files before executing features
         backtrace: false,   // <boolean> show full backtrace for errors
         compiler: [],       // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
         dryRun: false,      // <boolean> invoke formatters without executing steps
@@ -48,7 +54,7 @@ exports.config = {
         strict: false,      // <boolean> fail if there are any undefined or pending steps
         tags: [],           // <string[]> (expression) only execute the features or scenarios with tags matching the expression
         timeout: 20000,     // <number> timeout for step definitions
-        ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
+        ignoreUndefinedDefinitions: true, // <boolean> Enable this config to treat undefined definitions as warnings.
     },
     
     before: function() {
